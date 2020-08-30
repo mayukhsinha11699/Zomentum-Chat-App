4814dbf1e41220248a30a55c278f501ced874e1c
Mayukh <mayukh@Mayukhs-MacBook-Pro.local>
2020-08-31 02:07:18 +0530

    Added README.md, refactored code

---
+++ README.md
@@ -0,0 +1,31 @@
+# Zomentum Chat App (Front End Project)
+
+Website is hosted
+ [here ](https://mayukhsinha11699.github.io/Zomentum-Chat-App/)and is live using github pages.
+
+URL:  https://mayukhsinha11699.github.io/Zomentum-Chat-App/
+
+
+
+
+## Website Description
+
+This website is coded as a **take home assignment given by Zomentum.** The inspiration for this project is **web.whatsapp dark mode** which was launched recently.
+This project is made using HTML CSS(Grids, flexBox) and Javascript.
+
+This was supposed to be a **Front End Project** hence only the UI features are implemented. Although this website is meant to be used on a big screen, it is  made responsive for every device.
+
+I could have used ReactJS (or any other framework) but the purpose of the project was to create a responsive UI and I used Vanilla css to keep things simple.
+
+
+
+## Screenshot
+
+![](images/screenshot.png)
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+If you like this repo dont forget to star it :p
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+//THE ADD TO CHAT (on clicking send or pressing enter) FEATURE IS NOT IMPLEMENTED YET
+
 let x = "This function is not available yet. Sorry I need more time to code it";
 
-//js code to disable enter to avoid overflow
+//js code to disable enter to avoid overflow and to present the text
 $('div[contenteditable]').keydown(function(e) {
 
     if (e.keyCode === 13) {
@@ -10,26 +12,12 @@ $('div[contenteditable]').keydown(function(e) {
     }
 });
 
+//send alert on pressing the send button
+sendButton.addEventListener("click", function(){
+    alert(x);
+});
 
+// get element inside div dynamically?
 let sendButton = document.querySelector("#submit-button");
 let chatMessageList = document.querySelector("#chat-message-list");
 let t = document.getElementById('message-box').textContent;
-
-
-sendButton.addEventListener("click", function(){
-        alert(x);
-});
-
-
-{/* <div class="message-row you-message">
-                <div class="message-text">
-                    To sure calm much most long me mean. Able rent long in do we.
-                    Uncommonly no it announcing melancholy an in. Mirth learn it he given.
-                Secure shy favour length all twenty denote. He felicity no an at packages answered opinions juvenile. 
-
-                    Pasture he invited mr company shyness. But when shot real her.
-                    Chamber her observe visited removal six sending himself boy.
-                    At exquisite existence if an oh dependent excellent. Are gay head need down draw. 
-                    Misery wonder enable mutual get set oppose the uneasy. End why melancholy estimating her
-                     had indulgence middletons. Say ferrars demands besides her address. Blind going you merit few fancy their. 
-                </div> */}
